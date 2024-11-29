13b26cf35e11d03e601614ca972b5491177594f8
Samman Baral <112367663+SammanBaral@users.noreply.github.com>
2024-11-29 14:24:10 +0545

    dashboard added now

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,14 @@
+import 'package:first_flutter_apps/view/dashboard_view.dart';
 import 'package:flutter/material.dart';
-// import 'package:first_flutter_apps/view/first_view.dart';
-import 'package:first_flutter_apps/view/arithmetic_view.dart';
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ArithmeticView(),
+      home: DashboardView(),
     );
   }
 }
